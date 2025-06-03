9d87ba1258faa8e5b41f03a3874a8b6272d02dfb
Marian <getmoodyup@gmail.com>
2025-06-03 11:34:35 +0200

    nice-gadgets

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,42 @@
-import React from 'react';
 import './App.scss';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HomePage } from './modules/HomePage';
+import { PageNotFound } from './modules/PageNotFound';
+import { Header } from './components/Header';
+import { CategoryProduct } from './modules/CategoryProduct';
+import { Footer } from './components/Footer';
+import { ProductDetailsPage } from './modules/ProductDetailsPage';
+import { FavoritesPage } from './modules/FavoritesPage';
+import { CartPage } from './modules/CartPage';
+import { CartProvider } from './contexts/CartContext/CartContext';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
+export const App = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
+    <>
+      <Router>
+        <CartProvider>
+          <div className="App">
+            <Header />
+
+            {/* routes */}
+            <main className="main">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/favorites" element={<FavoritesPage />} />
+                <Route
+                  path="/:category/:productId"
+                  element={<ProductDetailsPage />}
+                />
+                <Route path="/:category" element={<CategoryProduct />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="*" element={<PageNotFound />} />
+              </Routes>
+            </main>
+          </div>
+
+          <Footer />
+        </CartProvider>
+      </Router>
+    </>
   );
 };
